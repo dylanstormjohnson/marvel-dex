24ae1df2b715070de258277145d7d45b00fa510a
EFP18 <ester.p18@hotmail.com>
2023-04-03 19:33:29 -0500

    created footer for copyrights

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -9,3 +9,16 @@
 //   window.location.replace(`./${character[i]}.html`)
 // }
 // }
+var apiKey = "d2cfd98c8f587c9ae382ce0a8ada3b38";
+var copyrights = $("#copyrights");
+
+async function getCopyrights(copyrights) {
+  var queryURL = "http://gateway.marvel.com/v1/public/characters?" + "&apikey=" + apiKey;
+  var rawData = await fetch(queryURL)
+  var data = await rawData.json()
+  var copyrightText = data.attributionText
+  copyrights.text(copyrightText)
+  console.log(copyrightText)
+}
+
+getCopyrights(copyrights)
