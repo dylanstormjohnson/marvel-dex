3e84a0a1d9366eb472217df3029716287d7432e0
Drew Smith <drgh056@gmail.com>
2023-04-03 19:50:10 -0500

    created homepage script

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,11 +11,8 @@
 // }
 var apiKey = "d2cfd98c8f587c9ae382ce0a8ada3b38";
 var copyrights = $("#copyrights");
-var xForceImg = $("#imgXForce");
-var xMenImg = $("#imgXMen");
-var avengersImg = $("#imgAvengers");
-var guardiansImg = $("#imgGuardians");
-var avengersComicID = "106048"
+
+
 
 async function getCopyrights(copyrights) {
   var queryURL = "http://gateway.marvel.com/v1/public/characters?" + "&apikey=" + apiKey;
@@ -26,36 +23,5 @@ async function getCopyrights(copyrights) {
   console.log(copyrightText)
 }
 
-// Gets list of comics to review for pic
-async function getSeriesData(charId) {
-    var queryURL = "https://gateway.marvel.com:443/v1/public/characters/" + charId + "/comics?apikey=" + apiKey;
-    var rawData = await fetch(queryURL)
-    /* If API call fails, */
-    // if (!rawData.ok) {
-    //     console.log("Whoops")
-    //     return;
-    // }
-    var data = await rawData.json()
-    console.log(data)
-    var imgURL = data.data.results[0].images[0].path + ".jpg"
-    avengersImg.attr("src", imgURL)
-}
 
-async function getComicData(comicId) {
-    var queryURL = "https://gateway.marvel.com:443/v1/public/comics/" + comicId + "?apikey=" + apiKey
-    var rawData = await fetch(queryURL)
-    /* If API call fails, */
-    // if (!rawData.ok) {
-    //     console.log("Whoops")
-    //     return;
-    // }
-    var data = await rawData.json()
-    console.log(data)
-    var imgURL = data.data.results[0].images[0].path + ".jpg"
-    avengersImg.attr("src", imgURL)
-}
-
-// getSeriesData(capnID);
-getComicData(avengersComicID)
-
-getCopyrights(copyrights)
+getCopyrights(copyrights);
