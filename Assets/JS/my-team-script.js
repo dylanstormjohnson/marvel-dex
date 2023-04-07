5827e33a7e7a7192d0f93fca3709f912a524ad6c
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-04-06 21:43:55 -0500

    added local storage function, my-team script and script script

--- Assets/JS/my-team-script.js
+++ Assets/JS/my-team-script.js
@@ -1,17 +1,18 @@
-var myTeam = ['duderinos', 'storm', "hi"]
+var myTeamArr = {
+  teamName: "", 
+  teamComp: []
+}
 var teamPick = $('.team')
 var characterContainerDivEl = $("#character-container");
 
-//localStorage.getItem('')
-
 function init() {
-  //myTeam = JSON.parse(localStorage.getItem("myTeamCharacters")) 
+  var myTeam = JSON.parse(localStorage.getItem("myTeamCharacters")) || myTeamArr
 
-  // for the length of my team, add a remove button under the images to then select a new character
-  for (var i = 0; i < myTeam.length; i++) {
-    var charName = myTeam[i]
+  for (var i = 0; i < myTeam.teamComp.length; i++) {
+    var charName = myTeam.teamComp[i]
     var html = `
     <div class="team col-3" id="playerOne">
+      <p>${charName}</p>
       <img src="./Assets/Images/Screenshot 2023-03-27 at 8.36.28 PM.png" class="img-fluid" alt="Marvel Character">
       <button class='remove-hero' >Remove Hero</button>
     </div>
@@ -22,8 +23,8 @@ function init() {
   }
 
   // for the remainder of empty spaces in my team, add a + sign to add more characters
-  for (var k = 0; k < 4 - myTeam.length; k++) {
-    var charName = myTeam[i]
+  for (var k = 0; k < 4 - myTeam.teamComp.length; k++) {
+    var charName = myTeam.teamComp[i]
     var html = `
     <div class="add-hero col-3 d-flex justify-content-center align-items-center">
     <i class="fa-solid fa-plus fa-2xl" style="font-size: 3rem" ></i>
@@ -41,7 +42,20 @@ init();
 // when the remove button is clicked, empty the space and create + sign to add another hero up to a max of 4
 characterContainerDivEl.on('click', '.remove-hero', function(event) {
   console.log('remove hero')
-  $(event.target).parent('.team').html('')
+  var myTeam = loadMyTeam();
+  var charName = $(event.target).siblings('p').text()
+
+  var newTeamComp = myTeam.teamComp.filter((val) => val !== charName);
+
+  myTeam.teamComp = newTeamComp;
+  saveMyTeam(myTeam)
+  
+  var html = `
+  <div class="add-hero col-3 d-flex justify-content-center align-items-center">
+  <i class="fa-solid fa-plus fa-2xl" style="font-size: 3rem" ></i>
+  </div>
+  ` 
+  $(event.target).parent('.team').html(html)
 });
 
 // add button leads back to home page to select new heros
@@ -49,6 +63,15 @@ characterContainerDivEl.on('click', '.add-hero', function(event) {
   console.log('add hero')
   window.location.href="../../index.html"
 });
+
+function loadMyTeam() {
+  var thisTeamArr = JSON.parse(localStorage.getItem("myTeamCharacters")) || myTeamArr
+  return thisTeamArr;
+}
+
+function saveMyTeam(myTeamArr) {
+  localStorage.setItem("myTeamCharacters", JSON.stringify(myTeamArr))
+}
 
 
 
