accf48d6d5c5858975220ec39b8109358d6a7ef0
Dylan Storm Johnson <dylanstormjohnson@comcast.net>
2023-04-08 08:00:45 -0500

    Thor/Yondu/Warpath Page Formatting, GoogleMaps API removing, etc

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,28 @@
 // var characters = ["Deadpool", "Warpath", "Wolverine", "Domino", "Magneto", "Storm", "Gambit", "Night Crawler", "Iron Man", "Captain America", "Thor", "Dr. Strange", "Groot", "Rocket", "Yondu", "Howard the Duck"];
+
+// window.redirect
+
+// get search input value
+// for (var i = 0; i < characters.length; i++){
+// if(searchInputValue == character[i]){
+//   window.location.replace(`./${character[i]}.html`)
+// }
+// }
+
+var copyrights = $("#copyrights");
+
+
+
+function getCopyrights(copyrights) {
+    console.log(copyrights)
+    var copyrightText = ` 
+        <a href='http://marvel.com'>Data provided by Marvel. © 2023 MARVEL</a> 
+        <p>Bio adapted from Wikipedia</p>
+        <p>Neither Marvel nor Wikipedia endorsed this project nor have collaborated to create this page. It is a student project. All characters, data, and images are copyright their respective owners</p> 
+    `
+    console.log(copyrightText)
+    copyrights.append(copyrightText)
+}
+
+getCopyrights(copyrights);
+
---
+++ Assets/JS/teams-pages.js
@@ -0,0 +1,71 @@
+var teamName = $("#teamName");
+var magneto = $("#magneto");
+var storm = $("#storm");
+var gambit = $("#gambit");
+var nightCrawler = $("#nightCrawler");
+var deadpool = $("#deadpool");
+var warpath = $("#warpath");
+var wolverine = $("#wolverine");
+var domino = $("#domino");
+var groot = $("#groot");
+var rocket = $("#rocket");
+var yondu = $("#yondu");
+var howardTheDuck = $("#howardTheDuck");
+var ironMan = $("#ironMan");
+var captainAmerica = $("#captainAmerica");
+var thor = $("#thor");
+var doctorStrange = $("#doctorStrange");
+
+
+// create a function to dynamically add characters on each team
+function marvelTeam() {
+  var charArr = [];
+  var idArr = [];
+  // var teams = ["X-Men", "X-Force", "Guardians of the Galaxy", "Avengers"];
+
+
+  // for (var j=0; j<teams.length; j++){
+  //   var teamNameH1El = $("<h1>")
+  //   teamNameH1El.text(teams[i])
+  //   teamName.append(teamNameH1El)
+
+
+  // check which team page we are in to choose the right characters array
+    console.log(teamName.text())
+    if (teamName.text() === "X-Men") {
+      idArr = [magneto, storm, gambit, nightCrawler]
+      charArr = ["Magneto", "Storm", "Gambit", "Night Crawler"]
+    } else if (teamName.text() === "X-Force") {
+      idArr = [deadpool, warpath, wolverine, domino]
+      charArr = ["Deadpool", "Warpath", "Wolverine", "Domino"]
+    } else if (teamName.text() === "Guardians of the Galaxy") {
+      idArr = [groot, rocket, yondu, howardTheDuck]
+      charArr = ["Groot", "Rocket", "Yondu", "Howard the Duck"]
+    } else if (teamName.text() === "Avengers") {
+      idArr = [ironMan, captainAmerica, thor, doctorStrange]
+      charArr = ["Iron Man", "Captain America", "Thor", "Doctor Strange"]
+    }
+  
+  // 4 characters in each team, create an h3, add an image, and a link to the bio page
+    for (var i=0; i<4; i++){
+      var charName = $("<h3>")
+      charName.text(charArr[i])
+      idArr[i].append(charName);
+
+
+      var charImg = $("<img>");
+      charImg.attr("src", "./Assets/Images/Screenshot 2023-03-27 at 8.36.28 PM.png")
+      charImg.addClass("img-fluid");
+      idArr[i].append(charImg);
+
+  console.log("href", "./" + idArr[i] + ".html")
+      var linkToPage = $("<a>");
+      linkToPage.text("Link to Bio");
+      linkToPage.attr("href", "./" + idArr[i].text() + ".html")
+      idArr[i].append(linkToPage);
+    
+    }
+  // }
+}
+
+marvelTeam()
